100354763c9700e82f838dedf27d8796326d9c74
brokkelkamp <rubenbrokkelkamp@gmail.com>
2020-02-26 17:35:33 +0100

    improved graph generating script

--- data/Synthetic/generate_random_graphs.py
+++ data/Synthetic/generate_random_graphs.py
@@ -3,6 +3,7 @@
 import networkx as nx
 import random
 import os
+from tqdm import tqdm
 
 import igraph
 
@@ -17,7 +18,10 @@ random.seed(SEED)
 MAX_EDGE_LENGTH = 1000
 
 
-def generate_queries(g, filename):
+def generate_queries_random(g, filename):
+    """ Generate queries by picking s and t at random and checking if they satisfy the requirements """
+
+    print("Generating queries for {}".format(filename))
     q = open(filename, "w")
     n = g.number_of_nodes()
     m = g.number_of_edges() 
@@ -63,6 +67,57 @@ def generate_queries(g, filename):
     q.close()
 
 
+def generate_queries(g, filename):
+    """ Generates queries by first finding all possible pairs s and t at a certain hop distance and then randomly
+    selecting a subset of those """
+
+    print("Generating queries for {}".format(filename))
+    q = open(filename, "w")
+    n = g.number_of_nodes()
+    m = g.number_of_edges() 
+
+
+    candidates = {i : [] for i in [2, 4, 6, 8]}
+
+    queries_per_category = 100
+
+    # generate queries at certain hop distance
+    for s in tqdm(range(n)):
+        nodes_at_dist_at_most = nx.single_source_shortest_path_length(g, source=s, cutoff=max(candidates))
+        
+        for t in nodes_at_dist_at_most:
+            if nodes_at_dist_at_most[t] in candidates:
+                candidates[nodes_at_dist_at_most[t]].append((s, t))
+
+    queries =  {}
+    for h in candidates:
+        if len(candidates[h]) < queries_per_category:
+            print("ERROR: Not enough candidate pairs for hop distance {}".format(h))
+        queries[h] = random.sample(candidates[h], queries_per_category)
+
+
+    queries[0] = []
+    # generate completely random queries
+    queries_so_far = 0
+    while queries_so_far < queries_per_category:
+        s = random.randrange(n)
+        t = random.randrange(n)
+        if s == t:
+            continue
+        try:
+            hops = nx.shortest_path_length(g, source=s, target=t)
+            if len(queries[0]) < queries_per_category and (s,t) not in queries[0]:
+                queries[0].append((s, t))
+                queries_so_far += 1
+
+        except nx.NetworkXNoPath:
+            continue
+    for hops in sorted(queries):
+        for s, t in queries[hops]:
+            q.write("{} {}\n".format(s, t))
+
+    q.close()
+
 
 # Another option to (faster) generate queries, but here the pairs won't be picked uniformly at random
 # First a start vertex is picked uniformly at random and then the end vertex is picked uniformly at random from
@@ -105,10 +160,10 @@ def generate_queries_skewed(g, filename):
         if s == t:
             continue
         try:
-            if len(queries[0]) < queries_per_category and (s,t) not in queries[0]:
+            hops = nx.shortest_path_length(g, source=s, target=t)
+            if (s,t) not in queries[0]:
                 queries[0].add((s, t))
                 queries_so_far += 1
-
         except nx.NetworkXNoPath:
             continue
     for hops in queries:
@@ -138,17 +193,24 @@ def ER(graph_sizes):
 
         generate_queries(g, "ER/ER_{}_{}.queries".format(n, m))
 
+        test_hop_distance("ER/ER_{}_{}".format(n, m))
 
 
 def BA(graph_sizes):
     print("Generating BA graphs")
     # https://igraph.org/python/doc/igraph.GraphBase-class.html#Barabasi
     for n in graph_sizes:
-        BA_m = 20 
+        BA_m = 2
 
         print("Generating BA graph with {} nodes".format(n))
         igraph_g = igraph.Graph.Barabasi(n, BA_m , directed=True)
         g = nx.DiGraph(igraph_g.get_edgelist())
+
+        #g_ba = nx.barabasi_albert_graph(n, 5)
+        #g = nx.DiGraph()
+        #g.add_edges_from(g_ba.edges())
+        ## also add the reverse edges
+        #g.add_edges_from([(v, u) for u,v in g_ba.edges()])
 
         m = g.number_of_edges()
 
@@ -157,63 +219,121 @@ def BA(graph_sizes):
         f = open("BA/BA_{}_{}.graph".format(n, m), "w")
         f.write("{} {}\n".format(n, m))
         for u, v in g.edges:
-            f.write("{} {} {} {}\n".format(u, v, random.randint(1, MAX_EDGE_LENGTH), random.random()))
+            l = random.randint(1, MAX_EDGE_LENGTH)
+            p = random.random()
+            f.write("{} {} {} {}\n".format(u, v, l, p))
         f.close()
 
         generate_queries(g, "BA/BA_{}_{}.queries".format(n, m))
 
+        test_hop_distance("BA/BA_{}_{}".format(n, m))
+
 def BP(graph_sizes):
     print("Generating BP graphs")
     for n in graph_sizes:
-        m = 2*n 
+        m = 2*n
 
-        print("Generating BP graph with {} nodes and {} edges".format(n, m))
+        g = nx.DiGraph()
+        g.add_nodes_from(range(n))
 
-        g = nx.algorithms.bipartite.generators.gnmk_random_graph(n//2, n//2, m, directed=True)
+        nodes_partition_1 = n//5
 
-        m = g.number_of_edges()
+        edges = 0
+        while edges < m:
+            u = random.randrange(nodes_partition_1)
+            v = random.randrange(nodes_partition_1, n)
 
-        # also add the reverse edges
-        g.add_edges_from([(v,u) for (u,v) in g.edges])
+            coin = random.random()
+            if coin < 0.5 and not g.has_edge(u, v):
+                g.add_edge(u, v)
+                edges += 1
+            elif not g.has_edge(v,u):
+                g.add_edge(v, u)
+                edges += 1
 
-        m = g.number_of_edges()
+        print("Generated BP graph with {} nodes and {} edges".format(n, m))
 
-        print("Graph has {} edges".format(m))
         f = open("BP/BP_{}_{}.graph".format(n, m), "w")
         f.write("{} {}\n".format(n, m))
         for u, v in g.edges:
-            f.write("{} {} {} {}\n".format(u, v, random.randint(1, MAX_EDGE_LENGTH), random.random()))
-            f.write("{} {} {} {}\n".format(v, u, random.randint(1, MAX_EDGE_LENGTH), random.random()))
+            l = random.randint(1, MAX_EDGE_LENGTH)
+            p = random.random()
+            f.write("{} {} {} {}\n".format(u, v, l, p))
         f.close()
 
-        generate_queries(g, "BP/BP_{}_{}.queries".format(n, m))
+        generate_queries_random(g, "BP/BP_{}_{}.queries".format(n, m))
 
-def SF(graph_sizes):
-    print("Generating SF graphs")
-    # https://igraph.org/python/doc/igraph.GraphBase-class.html
-    for n in graph_sizes:
+        test_hop_distance("BP/BP_{}_{}".format(n, m))
 
-        print("Generating SF graph with {} nodes".format(n))
-        g = nx.scale_free_graph(n)
-        m = g.number_of_edges()
+#def SF(graph_sizes):
+#    print("Generating SF graphs")
+#    # https://igraph.org/python/doc/igraph.GraphBase-class.html
+#    for n in graph_sizes:
+#
+#        print("Generating SF graph with {} nodes".format(n))
+#        g = nx.scale_free_graph(n)
+#        m = g.number_of_edges()
+#
+#        print("Graph has {} edges".format(m))
+#
+#        f = open("SF/SF_{}_{}.graph".format(n, m), "w")
+#        f.write("{} {}\n".format(n, m))
+#        for u, v, _ in g.edges:
+#            f.write("{} {} {} {}\n".format(u, v, random.randint(1, MAX_EDGE_LENGTH), random.random()))
+#        f.close()
+#
+#        generate_queries(g, "SF/SF_{}_{}.queries".format(n, m))
 
-        print("Graph has {} edges".format(m))
+def convert_edge_entry(x):
+    return [int(x[0]), int(x[1]), int(x[2]), float(x[3])]
 
-        f = open("SF/SF_{}_{}.graph".format(n, m), "w")
-        f.write("{} {}\n".format(n, m))
-        for u, v, _ in g.edges:
-            f.write("{} {} {} {}\n".format(u, v, random.randint(1, MAX_EDGE_LENGTH), random.random()))
-        f.close()
+def test_hop_distance(graph_name):
+    print("Testing {}".format(graph_name))
+    f_graph = open(graph_name + ".graph", "r")
+    f_queries = open(graph_name + ".queries", "r")
+    n, m = [int(x) for x in f_graph.readline().split()]
 
-        generate_queries(g, "SF/SF_{}_{}.queries".format(n, m))
+    g = nx.DiGraph()
+    g.add_nodes_from(range(n))
+    for _ in range(m):
+        u, v, l, p = convert_edge_entry(f_graph.readline().split())
+        if u < 0 or u >= n:
+            print("u is out of range in edge : {} {} {} {}".format(u, v, l, p))
+        if v < 0 or v >= n:
+            print("v is out of range in edge : {} {} {} {}".format(u, v, l, p))
+        if p < 0 or p > 1:
+            print("p is out of range in edge : {} {} {} {}".format(u, v, l, p))
+        g.add_edge(u, v)
+
+    # first 100 queries can be random, just check if they are within range
+    for hops in [0, 2, 4, 6, 8]:
+        for _ in range(100):
+            s, t = [int(x) for x in f_queries.readline().split()]
+            if s < 0 or s >= n:
+                print("s is out of range in query : {} {}".format(s, t))
+            if t < 0 or t >= n:
+                print("t is out of range in query : {} {}".format(s, t))
+            try:
+                hops_dist = nx.shortest_path_length(g, source=s, target=t)
+                if hops != 0 and hops_dist != hops:
+                    print("Nodes in query {} {} are {} hops apart while they should be {} hops apart".format(s, t,
+                        hops_dist, hops))
+
+            except nx.NetworkXNoPath:
+                print("No path between nodes in query : {} {}".format(s, t))
+                continue
+    print("Done testing".format(graph_name))
 
 
-graph_sizes = [10000, 20000, 50000, 100000, 500000, 1000000, 5000000, 10000000]
+
+
+graph_sizes = [10000, 20000, 50000, 100000] #, 500000, 1000000, 5000000, 10000000]
+
 
 ER(graph_sizes)
-#BA(graph_sizes)
-#BP(graph_sizes)
-#SF(graph_sizes)
+BA(graph_sizes)
+BP(graph_sizes)
+
 
     
 
