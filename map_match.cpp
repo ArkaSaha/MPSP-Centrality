0d1c8fbc9197be6d088dc1ddd78a8d0f32dcddb5
ArkaSaha <arkapravasaha.18@gmail.com>
2020-06-09 15:24:46 +0800

    More support for map_match

--- map_match.cpp
+++ map_match.cpp
@@ -24,7 +24,7 @@ using namespace std;
 using namespace osrm;
 using namespace json;
 
-void parse_beijing(char* file, EngineConfig& config)
+void parse_beijing_tdrive(char* file, EngineConfig& config)
 {
 	ifstream trajectory;
 	const OSRM osrm{config};
@@ -48,6 +48,76 @@ void parse_beijing(char* file, EngineConfig& config)
 				double longitude = stod(line.substr(p1+21),&p2);
 				double latitude = stod(line.substr(p1+21+p2+1),nullptr);
 				params.coordinates.push_back({util::FloatLongitude{longitude},util::FloatLatitude{latitude}});
+			}
+			trajectory.close();
+			params.annotations = true;
+			params.annotations_type = RouteParameters::AnnotationsType::All;
+			const auto status = osrm.Match(params,result);
+			auto& json_result = result.get<Object>();
+			if (status == Status::Ok)
+			{
+				auto& matches = json_result.values["matchings"].get<Array>();
+				double max_conf = 0;
+				int max_conf_idx = 0;
+				for (size_t j = 0; j < matches.values.size(); j++)
+				{
+					double conf = matches.values.at(j).get<Object>().values["confidence"].get<Number>().value;
+					if (conf > max_conf)
+					{
+						max_conf = conf;
+						max_conf_idx = j;
+					}
+				}
+				for (auto& matching : matches.values.at(max_conf_idx).get<Object>().values["legs"].get<Array>().values)
+				{
+					auto& match = matching.get<Object>().values["annotation"].get<Object>();
+					for (auto& node : match.values["nodes"].get<Array>().values)
+						cout << (unsigned long) node.get<Number>().value << "\t";
+					cout << endl;
+					for (auto& speed : match.values["speed"].get<Array>().values)
+						cout << speed.get<Number>().value * 3.6 << "\t";
+					cout << endl;
+				}
+			}
+			else
+			{
+				cerr << path << endl;
+				const auto code = json_result.values["code"].get<String>().value;
+				const auto message = json_result.values["message"].get<String>().value;
+				cerr << "Code: " << code << endl;
+				cerr << "Message: " << message << endl;
+			}
+		}
+	}
+}
+
+void parse_china_geolife(char* file, EngineConfig& config)
+{
+	ifstream trajectory;
+	const OSRM osrm{config};
+	for (auto& entry : filesystem::recursive_directory_iterator(file))
+	{
+		string path = entry.path().string();
+		if (path.substr(path.size()-4) == ".plt")
+		{
+			MatchParameters params;
+			engine::api::ResultT result = Object();
+			trajectory.open(path);
+			string line;
+			for (int i = 1; i <= 6; i++)
+				getline(trajectory,line);
+			while (getline(trajectory,line))
+			{
+				string::size_type p1, p2, p3, p4;
+				double latitude = stod(line,&p1);
+				double longitude = stod(line.substr(p1+1),&p2);
+				params.coordinates.push_back({util::FloatLongitude{longitude},util::FloatLatitude{latitude}});
+				stod(line.substr(p1+1+p2+3),&p3);
+				stod(line.substr(p1+1+p2+3+p3+1),&p4);
+				tm t = {};
+				istringstream ss(line.substr(p1+1+p2+3+p3+1+p4+1,19));
+				if (ss >> get_time(&t, "%Y-%m-%d,%H:%M:%S"))
+					params.timestamps.push_back(static_cast<unsigned long>(mktime(&t)));
 			}
 			trajectory.close();
 			params.annotations = true;
@@ -314,8 +384,9 @@ int main(int argc, char* argv[])
 	config.use_shared_memory = false;
 	config.algorithm = EngineConfig::Algorithm::MLD;
 	// parse_porto(argv[2],config);
-	// parse_beijing(argv[2],config);
+	// parse_beijing_tdrive(argv[2],config);
+	parse_china_geolife(argv[2],config);
 	// parse_sf_csd(argv[2],config);
-	parse_sf_mcd(argv[2],config);
+	// parse_sf_mcd(argv[2],config);
 	return EXIT_SUCCESS;
 }
