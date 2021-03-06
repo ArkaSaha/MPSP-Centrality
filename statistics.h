0e09d6274b91470067266a3beb6a5ceecb905f26
ArkaSaha <arkapravasaha.18@gmail.com>
2021-03-06 17:53:44 +0800

    Baseline

---
+++ statistics.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <ctime> 
+
+struct Statistics{
+    double candidate_generation = 0;
+    double probability_computation = 0;
+    bool candidate_generation_timeout = false;
+
+};
+
